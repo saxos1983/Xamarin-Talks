a96358d63201fea788e11b7786e9613c0499c867
Antoine Hauck <antoine.hauck@bbv.ch>
2015-04-10 10:10:29 +0200

    Adding Android Unit Test project.
    - Tests are file linked from the Core Unit Test Project.

---
+++ Phoneword.Forms/Phoneword.Forms.AndroidTest/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Android.App;
+using Android.OS;
+using Xamarin.Android.NUnitLite;
+
+namespace Phoneword.Forms.AndroidTest
+{
+    [Activity(Label = "Phoneword.Forms.AndroidTest", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : TestSuiteActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            // The tests are linked from the Unit Test project into this assembly
+            // and then added to the runner.
+            AddTest(Assembly.GetExecutingAssembly());
+
+            // Once you called base.OnCreate(), you cannot add more assemblies.
+            base.OnCreate(bundle);
+        }
+    }
+}
